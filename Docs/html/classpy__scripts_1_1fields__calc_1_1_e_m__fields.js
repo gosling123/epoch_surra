54b991d0ab4e3e616e36d2e4182bd928acb3c55a
gosling123 <b.gosling@warwick.ac.uk>
2022-07-18 17:42:05 +0100

    update

---
+++ Docs/html/classpy__scripts_1_1fields__calc_1_1_e_m__fields.js
@@ -0,0 +1,17 @@
+var classpy__scripts_1_1fields__calc_1_1_e_m__fields =
+[
+    [ "__init__", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a78c5a0b7082ddcbc24f670f7aa287bcd", null ],
+    [ "get_2D_Electric_Field_x", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a4bbfaa05aa493d6ef4560be3c5d4bdb9", null ],
+    [ "get_2D_Electric_Field_y", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a434737b58956877a47efee00b416c62f", null ],
+    [ "get_2D_FFT", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#ab7fb969c9830131aa60849e6aa9aa3e9", null ],
+    [ "get_2D_Magnetic_Field_z", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a3269c893575098cec26c5b280f21a22f", null ],
+    [ "get_filtered_signals", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#ade33ca0e0540faa03e8f635005424d1d", null ],
+    [ "get_flux", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#aedb2d5e7cfa71f3e7affc352066e514c", null ],
+    [ "get_flux_grid_av", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#aaedd6642abe9ee2af5fcd729719de161", null ],
+    [ "get_space_FFT", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a43c42cbc2f204a4abf380203b12b2fec", null ],
+    [ "get_time_FFT", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a98cf316fefc1a56ef7fe6df1a6ff89fc", null ],
+    [ "directory", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a302e445d0fdc71272f408644c60835bc", null ],
+    [ "epoch_data", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a06bcbdf86cb4a1c67b2da3e9968307ba", null ],
+    [ "nx", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#a0d86405314f6006cf523a7cb3ae8b51c", null ],
+    [ "timesteps", "classpy__scripts_1_1fields__calc_1_1_e_m__fields.html#afbf20aea59646b2d2c378c1711951d15", null ]
+];
